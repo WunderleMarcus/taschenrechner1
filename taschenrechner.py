525f5796010aad3833c01912cd431e35cf3b39ce
MarcusWunderle <wunderle@skymail.de>
2026-04-13 10:57:51 +0200

    Feature: Division hinzugefügt

--- taschenrechner.py
+++ taschenrechner.py
@@ -7,3 +7,10 @@ def multiply(a, b)
     return a * b
 
 print (multiply(2, 3))
+
+def divide(a, b):
+    if b == 0:
+        return "Error: Division by zero"
+    return a / b
+
+print (divide(6, 3))
